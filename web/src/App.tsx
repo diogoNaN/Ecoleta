927068bd5051b6695290514afee1a32f5cf75550
diogoNaN <diogoNaN@protonmail.ch>
2020-06-17 14:33:42 -0300

    front-end

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,28 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Front-End of Ecoleta Aplication
-        </p>
-        <p>
-          by <a
-          className="App-link"
-          href="https://github.com/diogoNaN"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          diogoNaN
-        </a>
-        </p>
-        
-      </header>
-    </div>
+    <Routes />
   );
 }
 
